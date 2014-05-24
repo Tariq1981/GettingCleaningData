6b44a570cb9b0f134e93a4900050967446bd4e9b
Tariq1981 <tariq.saeed81@gmail.com>
2014-05-24 20:19:26 +0200

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,71 +1,67 @@
-#Here is how to make a table with your variables that you can just paste into your markdown CodeBook.md file (I am using the Rstudio editor):
-  
-#  From R console, generate a data frame with the list of variable names from your tidy dataset, write to disk
+#Load reshape2 library which contains melt and dcast function 
+library(reshape2)
 
-#listOfVariables <- data.frame(names(tidyDataset))
-#write.csv(listOfVariables,"listOfVariables.csv")
-
-#Start a New Google Spreadsheet
-#Import your CSV file by File > Import > Upload (look for the file you just created)
-#Copy and paste into the markdown table generator at:http://www.tablesgenerator.com/markdown\_tables
-#Scroll down to the Generate button, press it.
-#Copy to clipboard (using the button provided).
-
-
-### Write comment in the Readme that using Camel convension doue to long variable names
-
-###1
+#Load activity levels lookup form its file.
 activity.levels<-read.table("activity_labels.txt")
 names(activity.levels)<-c("activity.id","activity.name")
 
+#Load the features names
 features.names<-read.table("features.txt")
 names(features.names)<-c("feature.id","feature.name")
 
+#Load the training and the testing dataset
 train.X<-read.table("train/X_train.txt")
 test.X<-read.table("test/X_test.txt")
 
+#Load the activity level Ids file for the training and the testing datasets.
 train.Y<-read.table("train/y_train.txt")
 test.Y<-read.table("test/y_test.txt")
 
+#Add the Activity Level name to the training and the testing dataset
 train.Y<-merge(train.Y,activity.levels,by.x="V1",by.y="activity.id")
 test.Y<-merge(test.Y,activity.levels,by.x="V1",by.y="activity.id")
 
+#Load the subjects Ids file for the training and the testing datasets.
 train.subject<-read.table("train/subject_train.txt")
 test.subject<-read.table("test/subject_test.txt")
 
+#Replace the default names for the training and teh testing datasets with the features names loaded in a previous step.
 names(train.X)<-features.names[,2]
 names(test.X)<-features.names[,2]
 
+#Add the Subject Id column to the training and the testing datasets.
 train.X$SubjectId<-train.subject[,1]
 test.X$SubjectId<-test.subject[,1]
-###3
+
+#Add the Activity Level Name column to the training and the testing datasets.
 train.X$ActivityLevel<-train.Y$activity.name
 test.X$ActivityLevel<-test.Y$activity.name
 
+#Adding a debugging column to idetifiy the training dataset from the testing dataset when they are merged in one dataset.
 train.X$DataType<-"Training"
 test.X$DataType<-"Testing"
-all.data<-rbind(train.X,test.X)
-#all.data$SubjectId<-as.factor(all.data$SubjectId)
-#all.data$DataType<-as.factor(all.data$DataType)
-#all.data$ActivityLevel<-as.factor(all.data$ActivityLevel)
 
-###2
+#Merging the training and testing datasets in one dataset called "all.data".
+all.data<-rbind(train.X,test.X)
+
+#Get Columns which contains mean or std measures in the created dataset into new dataset
 Mean.Std.Ind<-grep("\\-mean|\\-std|subjectid|activitylevel",tolower(names(all.data)))
 Mean.Std.Data<-all.data[,Mean.Std.Ind]
 
-###5
+#Reshaping the generated data set to generate the Mean for each Measure per Subject Id per Activity Level.
 MeltedData<-melt(Mean.Std.Data,c("SubjectId","ActivityLevel"))
-AvgData<-dcast(MeltedData,SubjectId+ActivityLevel~variable,mean,drop=F)
+Avg.Data<-dcast(MeltedData,SubjectId+ActivityLevel~variable,mean,drop=F)
 
-names(AvgData)<-gsub("Acc","Accelerometer",names(AvgData),fixed=T)
-names(AvgData)<-gsub("Gyro","Gyroscope",names(AvgData),fixed=T)
-names(AvgData)<-gsub("^t","TimeDomain",names(AvgData))
-names(AvgData)<-gsub("^f","FrequencyDomain",names(AvgData))
-names(AvgData)<-gsub("Mag","Magnitude",names(AvgData),fixed=T)
-names(AvgData)<-gsub("meanFreq","MeanFrequency",names(AvgData),fixed=T)
-names(AvgData)<-gsub("mean","Mean",names(AvgData),fixed=T)
-names(AvgData)<-gsub("std","StandardDeviation",names(AvgData),fixed=T)
-names(AvgData)<-gsub("-","",names(AvgData),fixed=T)
-names(AvgData)<-gsub("()","",names(AvgData),fixed=T)
-names(AvgData)[3:length(names(AvgData))]<-gsub("^","Mean",names(AvgData)[3:length(names(AvgData))])
-write.csv(AvgData,file="tidyData.csv",row.names = FALSE)
+#Renaming the measures to be appropriate names without special characters.
+names(Avg.Data)<-gsub("Acc","Accelerometer",names(Avg.Data),fixed=T)
+names(Avg.Data)<-gsub("Gyro","Gyroscope",names(Avg.Data),fixed=T)
+names(Avg.Data)<-gsub("^t","TimeDomain",names(Avg.Data))
+names(Avg.Data)<-gsub("^f","FrequencyDomain",names(Avg.Data))
+names(Avg.Data)<-gsub("Mag","Magnitude",names(Avg.Data),fixed=T)
+names(Avg.Data)<-gsub("meanFreq","MeanFrequency",names(Avg.Data),fixed=T)
+names(Avg.Data)<-gsub("mean","Mean",names(Avg.Data),fixed=T)
+names(Avg.Data)<-gsub("std","StandardDeviation",names(Avg.Data),fixed=T)
+names(Avg.Data)<-gsub("-","",names(Avg.Data),fixed=T)
+names(Avg.Data)<-gsub("()","",names(Avg.Data),fixed=T)
+names(Avg.Data)[3:length(names(Avg.Data))]<-gsub("^","Mean",names(Avg.Data)[3:length(names(Avg.Data))])
+write.csv(Avg.Data,file="tidyData.csv",row.names = FALSE)
